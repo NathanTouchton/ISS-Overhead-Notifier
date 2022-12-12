38382ecbf9b120d9458e6e17b7a1e17b704de7e7
Nathan Touchton <ntouchton@protonmail.com>
2022-12-11 18:47:08 -0600

    modified:   main.py

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from requests import get
 response = get(url="http://api.open-notify.org/iss-now.json")
 print(response)
 
-if response.status_code == 404:
-    raise Exception("This resource is not available.")
-if response.status_code == 401:
-    raise Exception("You are not authorized to access this data.")
+response.raise_for_status()
+
+latitude = response.json()["iss_position"]["latitude"]
+longitude = response.json()["iss_position"]["longitude"]
+iss_position = (latitude, longitude)
+
+print(iss_position)
