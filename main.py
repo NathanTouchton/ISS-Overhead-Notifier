b4dd321d4d2e5af09fda4061e6c6e43103fd81a3
Nathan Touchton <ntouchton@protonmail.com>
2022-12-16 13:34:36 -0600

    modified:   main.py

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from requests import get
 
-response = get(url="http://api.open-notify.org/iss-now.json")
-print(response)
+# response = get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
 
+# print(response)
+
+# latitude = response.json()["iss_position"]["latitude"]
+# longitude = response.json()["iss_position"]["longitude"]
+# iss_position = (latitude, longitude)
+# # print(response.json())
+
+# print(iss_position)
+
+
+parameters = {
+    "lat": 42.43,
+    "lng": -87.9
+}
+
+response = get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 
-latitude = response.json()["iss_position"]["latitude"]
-longitude = response.json()["iss_position"]["longitude"]
-iss_position = (latitude, longitude)
-
-print(iss_position)
+print(response.json())
